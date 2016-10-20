2e00b87c98e45cdfe419b0e7fd19e0a2b927e0d1
Sơn Đoàn <sondoan1204@gmail.com>
2016-10-20 20:24:15 +0700

    Upload

---
+++ DACN ver 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DACN_ver_2.Startup))]
+namespace DACN_ver_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
